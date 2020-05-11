c6bf77b6b520f4df6f0d401dc5ba4688d004174c
LL628 <413428442@qq.com>
2020-05-11 17:45:54 +0800

    Add fragment

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -5,6 +5,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweather.android.gson.Forecast;
 import com.coolweather.android.gson.Weather;
+import com.coolweather.android.service.AutoUpdateService;
 import com.coolweather.android.util.HttpUtil;
 import com.coolweather.android.util.Utility;
 
@@ -190,11 +192,17 @@ public class WeatherActivity extends AppCompatActivity {
         });
 
     }
+    /**
+     * 处理并展示Weather实体类中的数据
+     */
     private void showWeatherInfo(Weather weather){
         String cityName = weather.basic.cityName;
         String updateTime = weather.basic.update.updateTime.split(" ")[1];
         String degree = weather.now.temperature + "℃";
         String weatherInfo = weather.now.more.info;
+
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
 
         titleCity.setText(cityName);
         titleUpdateTime.setText(updateTime);
